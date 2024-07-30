9d8ddbbdca367301fbb3de3ae0120e1edbb1ca2b
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2024-07-30 15:55:45 +1200

    mongo db integrated , authentication method changed

---
+++ services/user-service.js
@@ -0,0 +1,53 @@
+// services/userService.js
+const connectDB = require('../config/db');
+const { ObjectId } = require('mongodb');
+
+function defineUserStructure(id, userData, userRole) {
+    return {
+        _id: id ? new ObjectId(id) : new ObjectId(),
+        username: userData.username,
+        role: userRole,
+        gender: userData.gender,
+        birthday: userData.birthday,
+        email: userData.email,
+        password: userData.password,
+    };
+}
+
+async function validateUserDetails(username, email) {
+    const db = await connectDB();
+    const collection = db.collection('users');
+
+    const user = await collection.findOne({
+        $or: [{ username }, { email }],
+    });
+    return user ? true : false;
+}
+
+async function createUserStudent(userData) {
+    const db = await connectDB();
+    const collection = db.collection('users');
+
+    const user = defineUserStructure(null, userData , "STUDENT");
+
+    const result = await collection.insertOne(user);
+    return result;
+}
+
+async function handleUserSignup(userDetails) {
+    const { username, email } = userDetails;
+
+
+    // Validate if username or email already exists
+    const userExists = await validateUserDetails(username, email);
+    if (userExists) {
+        throw new Error('Username or email already exists');
+    }
+
+
+    // Create and save new user
+    const newUser = await createUserStudent(userDetails);
+    return newUser;
+}
+
+module.exports = { validateUserDetails, createUserStudent, handleUserSignup };
