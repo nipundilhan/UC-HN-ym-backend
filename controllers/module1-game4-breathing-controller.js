e9544c4a570676addb6a1775874dbe9384db42c9
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2024-12-23 09:05:00 +1300

    requested changes part 2 commited

--- controllers/module1-game4-breathing-controller.js
+++ controllers/module1-game4-breathing-controller.js
@@ -32,4 +32,22 @@ async function getBreathingPracticesByStudentId(req, res) {
     }
 }
 
+// Delete breathing practice
+async function deleteBreathingPractice(req, res) {
+    try {
+        const { studentId, practiceId } = req.params; // Extract studentId and practiceId from the route parameters
+
+        const result = await breathingService.deleteBreathingPractice(studentId, practiceId);
+
+        if (result.modifiedCount > 0) {
+            res.status(200).json({ message: 'Breathing practice deleted successfully' });
+        } else {
+            res.status(404).json({ message: 'Breathing practice not found or already deleted' });
+        }
+    } catch (error) {
+        console.error('Error deleting breathing practice:', error);
+        res.status(400).json({ message: error.message });
+    }
+}
+
 module.exports = router;
