7891260bb9b36f08c7ac9343ad8afd792de54c30
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2025-02-17 17:12:22 +1300

    moods order fixed

--- services/modules-service.js
+++ services/modules-service.js
@@ -490,7 +490,7 @@ function generateCompleteMoods(moods, signupDate) {
     });
 
     // Order the moods with the latest first
-    return completeMoods;
+    return completeMoods.sort((a, b) => new Date(b.date) - new Date(a.date));;
 }
 
 async function shareBadge(data) {
