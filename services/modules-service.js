f1f99829c297808ecdc76011eaeee5a493d17f7a
Nipun Dilhan <nipundilhanjayarathne@gmail.com>
2026-01-07 13:20:35 +1300

    moods order issue fixed ( already ok in non gamified version)

--- services/modules-service.js
+++ services/modules-service.js
@@ -481,7 +481,7 @@ function generateCompleteMoods(moods, signupDate) {
         mood: mood.mood
     }));
 
-    /*
+ 
     // Get date range based on signup date
     const dateRange = getDateRange(signupDate);
 
@@ -493,8 +493,7 @@ function generateCompleteMoods(moods, signupDate) {
 
     // Order the moods with the latest first
     return completeMoods.sort((a, b) => new Date(b.date) - new Date(a.date));
-    */
-    return recordedMoods.sort((a, b) => new Date(b.date) - new Date(a.date));
+   
 }
 
 async function shareBadge(data) {
